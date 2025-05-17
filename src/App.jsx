3d1116970a7109d55a4772a51cca2b89a28d2776
MAXAI74 <sektor3@yandex.ru>
2025-05-18 02:22:40 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Добро пожаловать на maxai-site</h1>
+      <button>ВХОД</button>
+    </div>
+  );
+}
+
+export default App;
